0e6bbafbb9de715a06c28c4af78ba38ecd7f82f5
Torsten Bunde <github@torstenbunde.de>
2014-11-07 16:26:18 +0100

    Create statements_and_locks.sql
    SQL statement for showing blocked statements

---
+++ statements_and_locks.sql
@@ -0,0 +1,10 @@
+SELECT  bl.pid AS blocked_pid,
+	a.usename AS blocked_user,
+	kl.pid AS blocking_pid,
+	ka.usename AS blocking_user,
+	a.current_query AS blocked_statement
+  FROM pg_catalog.pg_locks bl
+    JOIN pg_catalog.pg_stat_activity a ON bl.pid = a.procpid
+    JOIN pg_catalog.pg_locks kl ON bl.transactionid = kl.transactionid AND bl.pid != kl.pid
+    JOIN pg_catalog.pg_stat_activity ka ON kl.pid = ka.procpid 
+  WHERE NOT bl.granted;
